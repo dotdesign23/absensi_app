f6e9818365d7e156f8946fe7e3be4130190ee6f1
dotdesign23 <61997056+dotdesign23@users.noreply.github.com>
2024-05-11 18:12:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,104 +1,104 @@
 import streamlit as st
 import pandas as pd
 
-# Cek apakah 'mahasiswa' sudah ada di session_state
-if 'mahasiswa' not in st.session_state:
-    st.session_state['mahasiswa'] = []
+# Cek apakah 'siswa' sudah ada di session_state
+if 'siswa' not in st.session_state:
+    st.session_state['siswa'] = []
 
 def tampilan_utama():
-    st.sidebar.title('Aplikasi Absensi Mahasiswa')
+    st.sidebar.title('Aplikasi Absensi Siswa')
 
-    menu = ['Kelola Mahasiswa', 'Kelola Absensi']
+    menu = ['Kelola Siswa', 'Kelola Absensi']
     pilihan = st.sidebar.selectbox('Menu', menu)
 
-    if pilihan == 'Kelola Mahasiswa':
-        kelola_mahasiswa()
+    if pilihan == 'Kelola Siswa':
+        kelola_siswa()
     elif pilihan == 'Kelola Absensi':
         kelola_absensi()
 
-def kelola_mahasiswa():
-    # Tamilan data mahasiswa
-    st.subheader('Kelola Mahasiswa')
+def kelola_siswa():
+    # Tamilan data siswa
+    st.subheader('Kelola Siswa')
 
-    # Mengecek apakah ada data mahasiswa
-    if len(st.session_state['mahasiswa']):
-        df = pd.DataFrame(st.session_state['mahasiswa'])
-        st.write(df[['Nama', 'NIM']])
+    # Mengecek apakah ada data siswa
+    if len(st.session_state['siswa']):
+        df = pd.DataFrame(st.session_state['siswa'])
+        st.write(df[['Nama', 'NIS']])
     else:
-        st.error('Belum ada data mahasiswa')
+        st.error('Belum ada data siswa')
     
-    # Tambah data mahasiswa
+    # Tambah data siswa
     with st.form(key='form_tambah'):
-        st.subheader('Tambah Data Mahasiswa')
+        st.subheader('Tambah Data Siswa')
 
-        # Meminta input nama & NIM
+        # Meminta input nama & NIS
         nama = st.text_input('Nama')
-        nim = st.text_input('NIM')
+        nis = st.text_input('NIS')
         submit_tambah = st.form_submit_button('Tambah')
         
         if submit_tambah:
 
             # Validasi input tidak boleh kosong
-            if nama == "" or nim == "":
-                st.error('Nama dan NIM tidak boleh kosong')
+            if nama == "" or nis == "":
+                st.error('Nama dan NIS tidak boleh kosong')
             
-            # Validasi NIM tidak boleh sama
-            elif any(mhs['NIM'] == nim for mhs in st.session_state['mahasiswa']):
-                st.error('NIM sudah ada')
+            # Validasi NIS tidak boleh sama
+            elif any(mhs['NIS'] == nis for mhs in st.session_state['siswa']):
+                st.error('NIS sudah ada')
 
             # Menambahkan data baru
             else:
-                st.session_state['mahasiswa'].append({'Nama': nama, 'NIM': nim, 'Absen': False})
+                st.session_state['siswa'].append({'Nama': nama, 'NIS': nis, 'Absen': False})
                 st.rerun()
 
-    # Hapus data mahasiswa
+    # Hapus data siswa
     with st.form(key='form_hapus'):
-        st.subheader('Hapus Data Mahasiswa')
+        st.subheader('Hapus Data Siswa')
 
-        # Meminta input NIM
-        nim_hapus = st.text_input('NIM yang akan dihapus')
+        # Meminta input NIS
+        nis_hapus = st.text_input('NIS yang akan dihapus')
         submit_hapus = st.form_submit_button('Hapus')
         
         if submit_hapus:
 
             # Validasi input tidak boleh kosong
-            if nim_hapus == "":
-                st.error('NIM tidak boleh kosong')
+            if nis_hapus == "":
+                st.error('NIS tidak boleh kosong')
 
-            # Mengubah status absensi mahasiswa
-            elif any(mhs['NIM'] == nim_hapus for mhs in st.session_state['mahasiswa']):
-                st.session_state['mahasiswa'] = [mhs for mhs in st.session_state['mahasiswa'] if mhs['NIM'] != nim_hapus]
+            # Mengubah status absensi siswa
+            elif any(mhs['NIS'] == nis_hapus for mhs in st.session_state['siswa']):
+                st.session_state['siswa'] = [mhs for mhs in st.session_state['siswa'] if mhs['NIS'] != nis_hapus]
                 st.rerun()
             
-            # Validasi NIM tidak ditemukan
+            # Validasi NIS tidak ditemukan
             else:
-                st.error('NIM tidak ditemukan')
+                st.error('NIS tidak ditemukan')
 
 def kelola_absensi():
     st.subheader('Kelola Absensi')
 
-    # Mengecek apakah ada data mahasiswa
-    if len(st.session_state['mahasiswa']):
-        df = pd.DataFrame(st.session_state['mahasiswa'])
+    # Mengecek apakah ada data siswa
+    if len(st.session_state['siswa']):
+        df = pd.DataFrame(st.session_state['siswa'])
         st.write(df)
     else:
-        st.error('Belum ada data mahasiswa')
+        st.error('Belum ada data siswa')
     
     with st.form(key='form_absensi'):
-        nim_absen = st.text_input('NIM')
+        nis_absen = st.text_input('NIS')
         submit_absen = st.form_submit_button('Absen')
 
         if submit_absen:
             # Validasi input tidak boleh kosong
-            if nim_absen == "":
-                st.error('NIM tidak boleh kosong')
+            if nis_absen == "":
+                st.error('NIS tidak boleh kosong')
             else:
-                for mhs in st.session_state['mahasiswa']:
-                    if mhs['NIM'] == nim_absen:
+                for mhs in st.session_state['siswa']:
+                    if mhs['NIS'] == nis_absen:
                         mhs['Absen'] = True
                         st.rerun()
-                        
-                st.error('NIM tidak ditemukan')
+
+                st.error('NIS tidak ditemukan')
 
 if __name__ == '__main__':
     tampilan_utama()
